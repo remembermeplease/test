d561fb5908188c0b94ce0951f29d0d1f965aa306
user <user@daseulkim>
2020-04-24 13:45:36 +0900

    한번 더 push

--- gitPrac/src/com/kh/gitPrac/Test.java
+++ gitPrac/src/com/kh/gitPrac/Test.java
@@ -5,16 +5,16 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("hello");
 		System.out.println("난 조장이다");
-<<<<<<< HEAD
+
 		System.out.println("111");
-=======
+
 		System.out.println("나는 조원이다");
-<<<<<<< HEAD
+
 	
 		System.out.println("zzzz");
-=======
->>>>>>> refs/remotes/origin/master
->>>>>>> refs/remotes/origin/master
+		System.out.println("ㅋㅋㅋ");
+	
+	
 	}
 
 }
